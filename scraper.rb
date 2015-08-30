11398d7163833ca24069c5c2215ca512499822be
Tony Bowden <tony@tmtm.com>
2015-08-30 08:28:00 +0100

    Changes at by-elections

--- scraper.rb
+++ scraper.rb
@@ -16,29 +16,59 @@ class String
   end
 end
 
+def date_from(str)
+  return if str.to_s.empty?
+  return Date.parse(str).to_s
+end
+
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
   # Nokogiri::HTML(open(url).read, nil, 'utf-8')
 end
 
-def scrape_term(t)
+def area_id(area)
+  'ocd-division/country:tv/constituency:%s' % area.downcase.tr(' ','-')
+end
+
+def scrape_term(t, changes)
   noko = noko_for(t[:source])
   noko.css('a[href*="Symbol_confirmed"]').each do |a|
     tds = a.xpath('ancestor::tr/td')
     area = a.xpath('ancestor::table//th[contains(.,"constituency results")]/a').text
-    area_id = 'ocd-division/country:tv/constituency:%s' % area.downcase.tr(' ','-')
     who = tds[1].css('a[href*="/wiki/"]')
 
     data = { 
       name: who.text,
       wikiname: who.attr('title').text,
       area: area,
-      area: area_id,
+      area_id: area_id(area),
       term: t[:id],
       source: t[:source]
     }
+    if (change = changes.find { |c| c[:term] == data[:term] && c[:area] == data[:area] && c[:replaced] == data[:name] })
+      data[:end_date] = change[:start_date]
+      data = [ data, change ]
+    end
     puts data
   # ScraperWiki.save_sqlite([:id, :term], data)
+  end
+end
+
+def scrape_byelections(url)
+  noko = noko_for(url)
+  noko.xpath('//h2[contains(.,"Tenth Parliament")]/following-sibling::table//tr[td[a]]').map do |tr|
+    tds = tr.css('td')
+    who_to = tds[5].css('a[href*="/wiki/"]')
+    data = { 
+      name: who_to.text,
+      wikiname: who_to.attr('title').text,
+      area: tds[0].text,
+      area_id: area_id(tds[0].text),
+      start_date: date_from(tds[1].text),
+      replaced: tds[3].text,
+      term: 10,
+      source: url,
+    }
   end
 end
 
@@ -66,5 +96,6 @@ terms = [
 ]
 # ScraperWiki.save_sqlite([:id], terms, 'terms')
 
-terms.each { |t| scrape_term(t) }
+changes = scrape_byelections('https://en.wikipedia.org/wiki/List_of_by-elections_in_Tuvalu')
+terms.each { |t| scrape_term(t, changes) }
 
